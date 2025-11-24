931dc810a1c9c6c45779a611064c11ed7c050dae
theatomheart <theatomheart@pm.me>
2025-11-24 19:51:53 +0000

    testing fixes

--- portscan.sh
+++ portscan.sh
@@ -5,19 +5,19 @@ Ports="T:3389,21,22,80,443"
 InputFile=$HOME/ips.csv
 
 Outpath=$HOME/.portscan		# This is the path the output will be saved to
+mkdir -p $Outpath
 
 if [ ! -f $InputFile ] ; then echo "The file $InputFile is missing." && exit 1 ; fi
 
 Output="$(date +%Y_%b_%d).md"
 
-mkdir -p $Outpath
->$Outpath/$Output
+#>$Outpath/$Output
 echo "Scanning for open ports: $Ports."
 while IFS="," read -r rec_column1 rec_column2
 do
   echo "Scanning $rec_column2 WAN IP $rec_column1."
-  echo "This is test output" >> $Outpath/scan.dmp
-  echo "nmap would have scanned $rec_column1 for the client $rec_column2" >> $Outpath/scan.dmp
+#  echo "This is test output" >> $Outpath/scan.dmp
+#  echo "nmap would have scanned $rec_column1 for the client $rec_column2" >> $Outpath/scan.dmp
 #  nmap -Pn -p "$Ports" "$rec_column1" > $Outpath/scan.dmp
 #  ScanPositive=$(cat $Outpath/scan.dmp | grep open | awk '{print $1}')
 #  if [ -z $ScanPositive ] ; then echo "$rec_column1 ($rec_column2) open port: $ScanPositive" >> $Outpath/$Output ; fi
