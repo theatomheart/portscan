57f95f3bfe99ea07d58f5e50e6630d6867ba1869
theatomheart <theatomheart@pm.me>
2025-11-24 20:00:42 +0000

    testing fixes

--- portscan.sh
+++ portscan.sh
@@ -16,10 +16,10 @@ echo "Scanning for open ports: $Ports."
 while IFS="," read -r rec_column1 rec_column2
 do
   echo "Scanning $rec_column2 WAN IP $rec_column1."
-  echo "This is test output" >> $Outpath/scan.dmp
-  echo "nmap would have scanned $rec_column1 for the client $rec_column2" >> $Outpath/scan.dmp
-#  nmap -Pn -p "$Ports" "$rec_column1" > $Outpath/scan.dmp
-#  ScanPositive=$(cat $Outpath/scan.dmp | grep open | awk '{print $1}')
-#  if [ -z $ScanPositive ] ; then echo "$rec_column1 ($rec_column2) open port: $ScanPositive" >> $Outpath/$Output ; fi
+#  echo "This is test output" >> $Outpath/scan.dmp
+#  echo "nmap would have scanned $rec_column1 for the client $rec_column2" >> $Outpath/scan.dmp
+  nmap -Pn -p "$Ports" "$rec_column1" > $Outpath/scan.dmp
+  ScanPositive=$(cat $Outpath/scan.dmp | grep open | awk '{print $1}')
+  if [ -z $ScanPositive ] ; then echo "$rec_column1 ($rec_column2) open port: $ScanPositive" >> $Outpath/$Output ; fi
 done < <(tail -n +2 $InputFile)
 #done < $InputFile
