b811b5cd00f375d08b5193dd1069c5658c4daa1e
theatomheart <theatomheart@pm.me>
2025-11-24 21:58:43 +0000

    script fixes

--- portscan.sh
+++ portscan.sh
@@ -2,20 +2,20 @@
 
 Ports="T:3389,21,22,80,443"
 
-InputFile=$HOME/ips.csv		# This is the CSV file with your list of IPs and names
+InputFile=$HOME/ips.csv		# This is the CSV file with your list of IPs and names.
 
-Outpath=$HOME/.portscan		# This is the path the output will be saved to
+Outpath=$HOME/.portscan		# This is the path the output will be saved to.
 
 Output="$(date +%Y_%b_%d).md"
 
-mkdir -p $Outpath
-
 if [ ! -f $InputFile ] ; then echo "The file $InputFile is missing." && exit 1 ; fi
 
+mkdir -p $Outpath
+
 echo "Scanning for open ports: $Ports."
+
 while IFS="," read -r rec_column1 rec_column2
 do
-#  echo "Scanning $rec_column2 WAN IP $rec_column1."
   nmap -Pn -p "$Ports" "$rec_column1" > $Outpath/scan.dmp
   ScanPositive=$(cat $Outpath/scan.dmp | grep open | awk '{print $1}')
   if [ ! -z $ScanPositive ] ; then echo "$rec_column1 ($rec_column2) open port: $ScanPositive" >> $Outpath/$Output ; fi
