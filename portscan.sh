890ef23cb30f53389fffe8a0b749f9fc6f321465
theatomheart <theatomheart@pm.me>
2025-11-25 17:47:39 +0000

    line 21 error

--- portscan.sh
+++ portscan.sh
@@ -16,7 +16,7 @@ echo "Scanning for open ports: $Ports."
 
 while IFS="," read -r rec_column1 rec_column2
 do
-  nmap -Pn -p "$Ports" "$rec_column1" > $Outpath/scan.dmp
+  nmap -Pn -p $Ports $rec_column1 > $Outpath/scan.dmp
   ScanPositive=$(cat $Outpath/scan.dmp | grep open | awk '{print $1}')
   if [ ! -z $ScanPositive ] ; then echo "$rec_column1 ($rec_column2) open port: $ScanPositive" >> $Outpath/$Output ; fi
 done < <(tail -n +2 $InputFile)
