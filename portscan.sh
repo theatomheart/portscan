00cbf15d0384a008e6c14b9951d66858924b5f89
theatomheart <theatomheart@pm.me>
2025-11-26 19:31:48 +0000

    output more md friendly

--- portscan.sh
+++ portscan.sh
@@ -13,12 +13,17 @@ if [ ! -f $InputFile ] ; then echo "The file $InputFile is missing." && exit 1 ;
 mkdir -p $Outpath
 
 echo "Scanning for open ports: $Ports."
+PrintPorts="${Ports:2}"
+echo "# Portscan looking for ports $PrintPorts" > $Outpath/$Output
 
 while IFS="," read -r rec_column1 rec_column2
 do
-  nmap -Pn -p $Ports $rec_column1 > $Outpath/scan.dmp
-  ScanPositive=$(cat $Outpath/scan.dmp | grep open | awk '{print $1}')
-  if [[ -n $ScanPositive ]] ; then echo "$rec_column1 ($rec_column2) open port: $ScanPositive" >> $Outpath/$Output ; fi
+	nmap -Pn -p $Ports $rec_column1 > $Outpath/scan.dmp
+	ScanPositive=$(cat $Outpath/scan.dmp | grep open | awk '{print $1}')
+	if [[ -n $ScanPositive ]] ; then
+		echo "$rec_column2 ($rec_column1) open port: $ScanPositive" >> $Outpath/$Output
+		echo "" >> $Outpath/$Output
+	fi
 done < <(tail -n +2 $InputFile)
 
 rm $Outpath/scan.dmp
