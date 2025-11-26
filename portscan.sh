3b2826302bf351b517b3f7cfc6335e32f31a3ab8
theatomheart <theatomheart@pm.me>
2025-11-26 19:54:00 +0000

    cleanup

--- portscan.sh
+++ portscan.sh
@@ -9,28 +9,21 @@ Outpath=$HOME/.portscan		# This is the path the output will be saved to.
 Output="$(date +%Y_%b_%d).md"
 
 Say() { echo "$*" >> $Outpath/$Output ;}
+
 if [ ! -f $InputFile ] ; then echo "The file $InputFile is missing." && exit 1 ; fi
 
-mkdir -p $Outpath
->$Outpath/$Output
+mkdir -p $Outpath ; >$Outpath/$Output
 
 PrintPorts="${Ports:2}"
 echo "Scanning for open ports: $PrintPorts."
-#echo "# Portscan looking for ports $PrintPorts" > $Outpath/$Output
-Say "# Portscan looking for ports $PrintPorts"
-#echo "" >> $Outpath/$Output
-Say ""
+Say "# Portscan looking for ports $PrintPorts" ; Say ""
 
-while IFS="," read -r rec_column1 rec_column2
-do
+while IFS="," read -r rec_column1 rec_column2 ; do
 	nmap -Pn -p $Ports $rec_column1 > $Outpath/scan.dmp
 	ScanPositive=$(cat $Outpath/scan.dmp | grep open | awk '{print $1}')
 	if [[ -n $ScanPositive ]] ; then
-#		echo "$rec_column2 ($rec_column1) open port: $ScanPositive" >> $Outpath/$Output
-		Say "$rec_column2 ($rec_column1) open port: $ScanPositive"
-#		echo "" >> $Outpath/$Output
-		Say ""
-	fi
+	  Say "$rec_column2 ($rec_column1) open port: $ScanPositive"
+	  Say "" ; fi
 done < <(tail -n +2 $InputFile)
 
 rm $Outpath/scan.dmp &>/dev/null
